5b3022ca3831d3c80aeaf78023532b38bfef3191
Nate del Rosario <ndelrosx6@gmail.com>
2023-07-12 13:10:24 -0700

    Addition: inference solutions from P3

--- Project-4-Su23-main/inference.py
+++ Project-4-Su23-main/inference.py
@@ -60,7 +60,6 @@ def constructBayesNet(gameState: hunters.GameState):
     edges = []
     variableDomainsDict = {}
 
-    "*** YOUR CODE HERE ***"
     # Constructing Bayes' nets: variables list
     variables += [PAC, GHOST0, GHOST1, OBS0, OBS1]
 
@@ -78,7 +77,6 @@ def constructBayesNet(gameState: hunters.GameState):
     variableDomainsDict[GHOST1]  = agent_domain.copy()
     variableDomainsDict[OBS0]  = obj_domain.copy()
     variableDomainsDict[OBS1]  = obj_domain.copy()
-    "*** END YOUR CODE HERE ***"
 
     net = bn.constructEmptyBayesNet(variables, edges, variableDomainsDict)
     return net
@@ -325,6 +323,61 @@ class DiscreteDistribution(dict):
         Return the sum of values for all keys.
         """
         return float(sum(self.values()))
+    
+    ########### ########### ###########
+    ########### QUESTION 5a ###########
+    ########### ########### ###########
+
+    def normalize(self):
+        """
+        Normalize the distribution such that the total value of all keys sums
+        to 1. The ratio of values for all keys will remain the same. In the case
+        where the total value of the distribution is 0, do nothing.
+
+        >>> dist = DiscreteDistribution()
+        >>> dist['a'] = 1
+        >>> dist['b'] = 2
+        >>> dist['c'] = 2
+        >>> dist['d'] = 0
+        >>> dist.normalize()
+        >>> list(sorted(dist.items()))
+        [('a', 0.2), ('b', 0.4), ('c', 0.4), ('d', 0.0)]
+        >>> dist['e'] = 4
+        >>> list(sorted(dist.items()))
+        [('a', 0.2), ('b', 0.4), ('c', 0.4), ('d', 0.0), ('e', 4)]
+        >>> empty = DiscreteDistribution()
+        >>> empty.normalize()
+        >>> empty
+        {}
+        """
+        "*** YOUR CODE HERE ***"
+        raiseNotDefined()
+        "*** END YOUR CODE HERE ***"
+
+    def sample(self):
+        """
+        Draw a random sample from the distribution and return the key, weighted
+        by the values associated with each key.
+
+        >>> dist = DiscreteDistribution()
+        >>> dist['a'] = 1
+        >>> dist['b'] = 2
+        >>> dist['c'] = 2
+        >>> dist['d'] = 0
+        >>> N = 100000.0
+        >>> samples = [dist.sample() for _ in range(int(N))]
+        >>> round(samples.count('a') * 1.0/N, 1)  # proportion of 'a'
+        0.2
+        >>> round(samples.count('b') * 1.0/N, 1)
+        0.4
+        >>> round(samples.count('c') * 1.0/N, 1)
+        0.4
+        >>> round(samples.count('d') * 1.0/N, 1)
+        0.0
+        """
+        "*** YOUR CODE HERE ***"
+        raiseNotDefined()
+        "*** END YOUR CODE HERE ***"
 
 
 class InferenceModule:
@@ -388,6 +441,18 @@ class InferenceModule:
         if agent == None:
             agent = self.ghostAgent
         return self.getPositionDistributionHelper(gameState, pos, index, agent)
+    
+    ########### ########### ###########
+    ########### QUESTION 5b ###########
+    ########### ########### ###########
+
+    def getObservationProb(self, noisyDistance: int, pacmanPosition: Tuple, ghostPosition: Tuple, jailPosition: Tuple):
+        """
+        Return the probability P(noisyDistance | pacmanPosition, ghostPosition).
+        """
+        "*** YOUR CODE HERE ***"
+        raiseNotDefined()
+        "*** END YOUR CODE HERE ***"
 
     def setGhostPosition(self, gameState, ghostPosition, index):
         """
@@ -478,6 +543,47 @@ class ExactInference(InferenceModule):
         for p in self.legalPositions:
             self.beliefs[p] = 1.0
         self.beliefs.normalize()
+    
+    ########### ########### ###########
+    ########### QUESTION 6  ###########
+    ########### ########### ###########
+
+    def observeUpdate(self, observation: int, gameState: busters.GameState):
+        """
+        Update beliefs based on the distance observation and Pacman's position.
+
+        The observation is the noisy Manhattan distance to the ghost you are
+        tracking.
+
+        self.allPositions is a list of the possible ghost positions, including
+        the jail position. You should only consider positions that are in
+        self.allPositions.
+
+        The update model is not entirely stationary: it may depend on Pacman's
+        current position. However, this is not a problem, as Pacman's current
+        position is known.
+        """
+        "*** YOUR CODE HERE ***"
+        raiseNotDefined()
+        "*** END YOUR CODE HERE ***"
+        self.beliefs.normalize()
+    
+    ########### ########### ###########
+    ########### QUESTION 7  ###########
+    ########### ########### ###########
+
+    def elapseTime(self, gameState: busters.GameState):
+        """
+        Predict beliefs in response to a time step passing from the current
+        state.
+
+        The transition model is not entirely stationary: it may depend on
+        Pacman's current position. However, this is not a problem, as Pacman's
+        current position is known.
+        """
+        "*** YOUR CODE HERE ***"
+        raiseNotDefined()
+        "*** END YOUR CODE HERE ***"
 
     def getBeliefDistribution(self):
         return self.beliefs
@@ -493,3 +599,65 @@ class ParticleFilter(InferenceModule):
 
     def setNumParticles(self, numParticles):
         self.numParticles = numParticles
+    
+    ########### ########### ###########
+    ########### QUESTION 9  ###########
+    ########### ########### ###########
+
+    def initializeUniformly(self, gameState: busters.GameState):
+        """
+        Initialize a list of particles. Use self.numParticles for the number of
+        particles. Use self.legalPositions for the legal board positions where
+        a particle could be located. Particles should be evenly (not randomly)
+        distributed across positions in order to ensure a uniform prior. Use
+        self.particles for the list of particles.
+        """
+        self.particles = []
+        "*** YOUR CODE HERE ***"
+        raiseNotDefined()
+        "*** END YOUR CODE HERE ***"
+
+    def getBeliefDistribution(self):
+        """
+        Return the agent's current belief state, a distribution over ghost
+        locations conditioned on all evidence and time passage. This method
+        essentially converts a list of particles into a belief distribution.
+
+        This function should return a normalized distribution.
+        """
+        "*** YOUR CODE HERE ***"
+        raiseNotDefined()
+        "*** END YOUR CODE HERE ***"
+    
+    ########### ########### ###########
+    ########### QUESTION 10 ###########
+    ########### ########### ###########
+
+    def observeUpdate(self, observation: int, gameState: busters.GameState):
+        """
+        Update beliefs based on the distance observation and Pacman's position.
+
+        The observation is the noisy Manhattan distance to the ghost you are
+        tracking.
+
+        There is one special case that a correct implementation must handle.
+        When all particles receive zero weight, the list of particles should
+        be reinitialized by calling initializeUniformly. The total method of
+        the DiscreteDistribution may be useful.
+        """
+        "*** YOUR CODE HERE ***"
+        raiseNotDefined()
+        "*** END YOUR CODE HERE ***"
+    
+    ########### ########### ###########
+    ########### QUESTION 11 ###########
+    ########### ########### ###########
+
+    def elapseTime(self, gameState):
+        """
+        Sample each particle's next state based on its current state and the
+        gameState.
+        """
+        "*** YOUR CODE HERE ***"
+        raiseNotDefined()
+        "*** END YOUR CODE HERE ***"
